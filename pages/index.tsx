5573dc9ac5d19be8b95c3121ffb3fdf0a3d1cd87
Chirag Taraviya <chirag.taraviya1986@gmail.com>
2026-07-23 17:10:20 +0530

    Json schema for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import type { PageOptions } from '@graphcommerce/framer-next-pages'
 import { cacheFirst } from '@graphcommerce/graphql'
 import { ProductListDocument } from '@graphcommerce/magento-product'
@@ -43,8 +44,165 @@ function CmsPage(props: CmsPageRouteProps) {
   const decodedHomeCta = decodeHtmlEntities(homeCtaData?.content)
   const decodedHomeCeleberations = decodeHtmlEntities(homeCeleberationsData?.content)
   const decodedHomeImagination = decodeHtmlEntities(homeImaginationData?.content)
+  const homePageSchema = {
+    '@context': 'https://schema.org',
+    '@graph': [
+      {
+        '@type': 'Organization',
+        '@id': 'https://www.rnbcakes.com/#organization',
+        name: 'Ribbons & Balloons',
+        alternateName: 'RNB Cakes',
+        url: 'https://www.rnbcakes.com/',
+        logo: 'https://srv900162.hstgr.cloud/media/.renditions/wysiwyg/cns-apge/footer/footer_logo.png',
+        description:
+          'Eggless cake shop in Dubai for birthdays, celebrations and gifting. Order fresh eggless cakes, custom designs, plus vegan and gluten-free, delivered citywide.',
+        contactPoint: {
+          '@type': 'ContactPoint',
+          telephone: '+971528899029',
+          contactType: 'customer service',
+          areaServed: 'AE',
+        },
+        sameAs: ['https://www.instagram.com/ribbonsandballoonsbakery/'],
+      },
+      {
+        '@type': 'WebSite',
+        '@id': 'https://www.rnbcakes.com/#website',
+        url: 'https://www.rnbcakes.com/',
+        name: 'Ribbons & Balloons',
+        publisher: { '@id': 'https://www.rnbcakes.com/#organization' },
+      },
+      {
+        '@type': 'Bakery',
+        '@id': 'https://www.rnbcakes.com/#bakery-oud-metha',
+        name: 'Ribbons & Balloons - Oud Metha',
+        parentOrganization: { '@id': 'https://www.rnbcakes.com/#organization' },
+        url: 'https://www.rnbcakes.com/',
+        image:
+          'https://srv900162.hstgr.cloud/media/.renditions/wysiwyg/cns-apge/footer/footer_logo.png',
+        telephone: '+97142698623',
+        priceRange: '$$',
+        currenciesAccepted: 'AED',
+        paymentAccepted: 'Cash, Visa, Mastercard',
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: 'Shop No. 5, 8th Street, Oud Metha',
+          addressLocality: 'Dubai',
+          addressCountry: 'AE',
+        },
+        geo: { '@type': 'GeoCoordinates', latitude: 25.2376178, longitude: 55.3059131 },
+        hasMap: 'https://maps.app.goo.gl/51NCwzXfyTTK5Aw19',
+        openingHoursSpecification: [
+          {
+            '@type': 'OpeningHoursSpecification',
+            dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            opens: '09:00',
+            closes: '23:00',
+          },
+        ],
+        sameAs: ['https://www.instagram.com/ribbonsandballoonsbakery/'],
+      },
+      {
+        '@type': 'Bakery',
+        '@id': 'https://www.rnbcakes.com/#bakery-studio-city',
+        name: 'Ribbons & Balloons - Studio City',
+        parentOrganization: { '@id': 'https://www.rnbcakes.com/#organization' },
+        url: 'https://www.rnbcakes.com/',
+        image:
+          'https://srv900162.hstgr.cloud/media/.renditions/wysiwyg/cns-apge/footer/footer_logo.png',
+        telephone: '+971524898141',
+        priceRange: '$$',
+        currenciesAccepted: 'AED',
+        paymentAccepted: 'Cash, Visa, Mastercard',
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: 'Dubai Studio City',
+          addressLocality: 'Dubai',
+          addressCountry: 'AE',
+        },
+        geo: { '@type': 'GeoCoordinates', latitude: 25.0408714, longitude: 55.2464491 },
+        hasMap: 'https://maps.app.goo.gl/uKagVd2v7a3BQbtx8',
+        openingHoursSpecification: [
+          {
+            '@type': 'OpeningHoursSpecification',
+            dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            opens: '09:00',
+            closes: '23:00',
+          },
+        ],
+        sameAs: ['https://www.instagram.com/ribbonsandballoonsbakery/'],
+      },
+      {
+        '@type': 'Bakery',
+        '@id': 'https://www.rnbcakes.com/#bakery-al-qusais',
+        name: 'Ribbons & Balloons - Al Qusais',
+        parentOrganization: { '@id': 'https://www.rnbcakes.com/#organization' },
+        url: 'https://www.rnbcakes.com/',
+        image:
+          'https://srv900162.hstgr.cloud/media/.renditions/wysiwyg/cns-apge/footer/footer_logo.png',
+        telephone: '+971528483368',
+        priceRange: '$$',
+        currenciesAccepted: 'AED',
+        paymentAccepted: 'Cash, Visa, Mastercard',
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: 'Inside Royal Medcare Hospital, 16 18th Street, Al Qusais 2',
+          addressLocality: 'Dubai',
+          addressCountry: 'AE',
+        },
+        geo: { '@type': 'GeoCoordinates', latitude: 25.267828, longitude: 55.3862615 },
+        hasMap: 'https://maps.app.goo.gl/AkX9yHAFhzbuRdD48',
+        openingHoursSpecification: [
+          {
+            '@type': 'OpeningHoursSpecification',
+            dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            opens: '09:00',
+            closes: '23:00',
+          },
+        ],
+        sameAs: ['https://www.instagram.com/ribbonsandballoonsbakery/'],
+      },
+      {
+        '@type': 'Bakery',
+        '@id': 'https://www.rnbcakes.com/#bakery-barsha-heights',
+        name: 'Ribbons & Balloons - Barsha Heights',
+        parentOrganization: { '@id': 'https://www.rnbcakes.com/#organization' },
+        url: 'https://www.rnbcakes.com/',
+        image:
+          'https://srv900162.hstgr.cloud/media/.renditions/wysiwyg/cns-apge/footer/footer_logo.png',
+        telephone: '+971588238753',
+        priceRange: '$$',
+        currenciesAccepted: 'AED',
+        paymentAccepted: 'Cash, Visa, Mastercard',
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: 'Inside Grosvenor Business Tower, Barsha Heights',
+          addressLocality: 'Dubai',
+          addressCountry: 'AE',
+        },
+        geo: { '@type': 'GeoCoordinates', latitude: 25.0994167, longitude: 55.1776793 },
+        hasMap: 'https://maps.app.goo.gl/yeFUpWe48KP7hpga8',
+        openingHoursSpecification: [
+          {
+            '@type': 'OpeningHoursSpecification',
+            dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            opens: '09:00',
+            closes: '23:00',
+          },
+        ],
+        sameAs: ['https://www.instagram.com/ribbonsandballoonsbakery/'],
+      },
+    ],
+  }
+
   return (
     <>
+      <Head>
+        <script
+          type='application/ld+json'
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(homePageSchema) }}
+        />
+      </Head>
       <PageMeta
         title='Cakes &amp; Custom Desserts | Dubai Online Bakery'
         metaDescription='Order handcrafted cakes, vegan &amp; gluten-free treats, brownies and personalized desserts in Dubai. Celebrate birthdays, weddings &amp; special moments with RNB Cakes!'
